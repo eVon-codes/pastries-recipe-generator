284e7e7b17da613af75f6818b649f8cb27eac483
eVon-codes <yvonnebukari25@gmail.com>
2025-07-16 17:04:20 +0200

    Added javascript form handling and typewriter plugins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
- 
+function generateRecipe(event) {
+  event.preventDefault();
+  new Typewriter("#recipe", {
+    strings: "Perfect let's get right into it...",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let recipeElement = document.querySelector("#recipe-generator-form");
+recipeElement.addEventListener("submit", generateRecipe);
