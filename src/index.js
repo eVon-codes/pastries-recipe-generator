dd96559774fd527b58458d7c818e324cca83fe66
eVon-codes <yvonnebukari25@gmail.com>
2025-07-16 18:28:01 +0200

    Added AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-function generateRecipe(event) {
-  event.preventDefault();
+//response to API call
+function displayRecipe(response) {
+  console.log("Recipe generated");
   new Typewriter("#recipe", {
-    strings: "Perfect let's get right into it...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "13o2bcb26t4d3e95f87ea28b60545401";
+  let context =
+    "You are a pastry chef expect and love to teach beginners about how to make mouth-watering pasteries using simple, and precise beginner friendly pastry recipes. Your mission is to generate not more than 15 lines of pastry recipes in basic HTML and seperate each line with a <br/>. Do not include the basic html or html word in your response. Make sure to follow the user instructions";
+  let prompt = `User instructions are: generate a beginner recipe and tutorial guide about${instructionsInput.value}`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe...");
+  console.log(`context:${context}`);
+  console.log(`prompt:${prompt}`);
+
+  //make a call to the API
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeElement = document.querySelector("#recipe-generator-form");
